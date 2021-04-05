4da5f92d25fd7ad4d5e037874fa2bb287a3a5884
MungGame <gfdbvc123@gmail.com>
2021-04-05 22:59:27 +0900

    AJAX PREC
    AJAX

---
+++ AJAX/color.js
@@ -0,0 +1,36 @@
+var Body = {
+  setColor:function(color) {
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor:function(color) {
+    document.querySelector('body').style.backgroundColor = color;
+  }
+}
+
+var Links = {
+  setColor:function(color) {
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i < alist.length) {
+      alist[i].style.color = color;
+      ++i;
+    }
+  }
+}
+
+function nightdayhandler(self) {
+  if(self.value === 'night') {
+    Body.setColor('white');
+    Body.setBackgroundColor('black');
+    self.value = 'day';
+
+    Links.setColor('yellow');
+  }
+  else {
+    Body.setColor('black');
+    Body.setBackgroundColor('white');
+    self.value = 'night';
+
+    Links.setColor('blue');
+  }
+}
